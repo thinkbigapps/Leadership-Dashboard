61452809301518a5e4e3edc8739e766bcbae255c
mason-allen <mason.allen@gmail.com>
2017-08-27 15:16:37 -0600

    Re-adding consultation card images to repo

---
+++ DataAccess/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    class DatabaseConnection
+    {
+        //db connection information removed for security
+        const string ConnectionString = @"Server=*Enter Host*;uid=*Enter Username*;pwd=*Enter Password*;database=ex_eventDB";
+
+        static public SqlConnection GetExEventDatabaseConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
